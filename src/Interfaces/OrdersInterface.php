af66b82f98cab84db62a4e56d750ee38246a63d2
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2022-08-18 01:11:42 +0200

    Updated OrdersInterface::orders()

--- src/Interfaces/OrdersInterface.php
+++ src/Interfaces/OrdersInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace JeffreyKroonen\BolRetailer\Interfaces;
 
+use JeffreyKroonen\BolRetailer\Enums\Orders\FulfilmentMethods;
+use JeffreyKroonen\BolRetailer\Enums\Orders\StatusTypes;
+use JeffreyKroonen\BolRetailer\Utilities\Paginate;
+
 interface OrdersInterface
 {
-    public function orders();
+    public function orders(int $page, FulfilmentMethods $fulfillmentMethod, StatusTypes $status): Paginate;
 }
