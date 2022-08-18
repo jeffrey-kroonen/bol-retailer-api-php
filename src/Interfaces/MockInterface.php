5efa9131eda6031c5c6ab1bec24efe4e64681dd0
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2022-08-18 08:26:32 +0200

    Added MockInterface

---
+++ src/Interfaces/MockInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeffreyKroonen\BolRetailer\Interfaces;
+
+interface MockInterface
+{
+    public const MOCK_CREDENTIALS = 'mock_credentials';
+    public const MOCK_CLIENT_ID = 'mock_client_id';
+    public const MOCK_CLIENT_SECRET = 'mock_client_secret';
+    public const MOCK_ACCESS_TOKEN = 'mock_access_token';
+}
