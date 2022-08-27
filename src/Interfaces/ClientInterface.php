e6daf16e72c92edf9e9f5ee055d577bca8700138
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2022-08-27 11:35:11 +0200

    Added ClientInterface interface

---
+++ src/Interfaces/ClientInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeffreyKroonen\BolRetailer\Interfaces;
+
+use JeffreyKroonen\BolRetailer\Endpoints\Orders;
+use JeffreyKroonen\BolRetailer\Endpoints\Returns;
+use JeffreyKroonen\BolRetailer\Endpoints\Subscriptions;
+use JeffreyKroonen\BolRetailer\Utilities\Auth;
+
+interface ClientInterface
+{
+    public function setAuth(Auth $auth): self;
+    public function orders(): Orders;
+    public function returns(): Returns;
+    public function subscriptions(): Subscriptions;
+}
